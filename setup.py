f11db6663f3b124d08a6d2fbbd0c04b784519048
rv <vjc.robin@gmail.com>
2023-07-02 17:06:49 +1000

    updated repo version to try and run on colab new package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", encoding="utf-8") as f:
 
 setup(
     name="leonardo",
-    version="1.0",
+    version="1.1",
     description="Leonardo - Image processing using Stable Difussion Models",
     author="Robin Vujanic",
     author_email="vjc.robin@gmail.com",
